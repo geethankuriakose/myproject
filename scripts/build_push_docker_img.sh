027a9730e4d0158eeed07a1c5dd6598247720d79
geethankuriakose <geethankuriakose@yahoo.com>
2021-02-17 11:27:53 +0530

    Create build_push_docker_img.sh

---
+++ scripts/build_push_docker_img.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo docker images
+sudo docker build -t sosamma2018/myapp:v10 .
+sudo docker push sosamma2018/myapp:v10
+sudo docker build -t sosamma2018/myapp:v11 .
+sudo docker push sosamma2018/myapp:v11
+sudo docker images
+#docker run  -p 8000:8000 --name mydemoapp  myapp:v11
+#curl localhost:8000
+#docker exec -it mydemoapp -- /bin/bash
